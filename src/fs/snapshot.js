690f4b20b2879c9ec0540f34d867018dd1427a04
Alina Vagner <ali.vag977@gmail.com>
2026-03-04 16:34:33 +0300

    create snapshot

--- src/fs/snapshot.js
+++ src/fs/snapshot.js
@@ -1,9 +1,76 @@
+import fs from "fs";
+import path from "path";
+
+//Expected, that the directory "workspace" will be in the "npde-nodejs-fundamentals" directory
+//Otherwise, it will be created there
+
 const snapshot = async () => {
-  // Write your code here
-  // Recursively scan workspace directory
-  // Write snapshot.json with:
-  // - rootPath: absolute path to workspace
-  // - entries: flat array of relative paths and metadata
+  try {
+    function getEntries(rootPath) {
+      const result = [];
+
+      fs.readdirSync(rootPath, { recursive: true })
+        .map((elem) => {
+          return { path: elem, stats: fs.statSync(`${rootPath}/${elem}`) };
+        })
+        .forEach(({ path, stats }) => {
+          const type = stats.isFile()
+            ? "file"
+            : stats.isDirectory()
+              ? "directory"
+              : null;
+
+          if (type === "file") {
+            result.push({
+              path,
+              type,
+              size: stats.size,
+              content: fs.readFileSync(`${rootPath}/${path}`, {
+                encoding: "utf8",
+              }),
+            });
+          }
+          if (type === "directory") {
+            result.push({
+              path,
+              type,
+            });
+          }
+        });
+
+      return result;
+    }
+
+    const directoryPath = "./workspace";
+
+    // Get workspace
+    if (fs.existsSync(directoryPath)) {
+      console.log("The directory exists");
+    } else {
+      console.log("The directory does NOT exist");
+
+      fs.mkdirSync(directoryPath);
+      return;
+    }
+
+    // Get data and create JSON
+    const data = {
+      rootPath: path.resolve(directoryPath),
+      entries: getEntries(directoryPath),
+    };
+
+    const jsonData = JSON.stringify(data, null, 2);
+    const promise = fs.writeFile("snapshot.json", jsonData, (err) => {
+      if (err) {
+        return new Error("FS operation failed");
+      }
+      console.log("File has been written successfully!");
+    });
+
+    return promise;
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await snapshot();
