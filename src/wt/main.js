be8a4c66a5d30093bf1c1d05b9f1f5e35e774948
Alina Vagner <ali.vag977@gmail.com>
2026-03-08 05:07:06 +0300

    add worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,11 +1,51 @@
+import { Worker, isMainThread, parentPort } from "node:worker_threads";
+import { cpus } from "node:os";
+import { checkDirectory } from "../helpers.js";
+import { readFile } from "node:fs/promises";
+
+function runWorker(data) {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker("./src/wt/worker.js");
+    worker.postMessage(data);
+
+    worker.on("message", (data) => resolve(data));
+    worker.on("error", reject);
+    worker.on("exit", (code) => {
+      if (code !== 0) reject(new Error(`Worker stopped with ${code}`));
+    });
+  });
+}
+
 const main = async () => {
-  // Write your code here
-  // Read data.json containing array of numbers
-  // Split into N chunks (N = CPU cores)
-  // Create N workers, send one chunk to each
-  // Collect sorted chunks
-  // Merge using k-way merge algorithm
-  // Log final sorted array
+  const cores = cpus().length;
+  const result = [];
+  const directoryPath = `${process.cwd()}/workspace/data.json`;
+
+  console.log(cores);
+  try {
+    await checkDirectory(directoryPath);
+
+    const data = JSON.parse(
+      await readFile(directoryPath, { encoding: "utf8" }),
+    );
+    console.log(typeof data);
+    const chunkQ = Math.floor(data.length / cores);
+    const output = new Array(cores);
+    let cur = 0;
+
+    for (let i = 0; i < cores; i++) {
+      output[i] = data.slice(cur * chunkQ, (cur + 1) * chunkQ);
+      cur++;
+    }
+
+    const results = await Promise.all(output.map((data) => runWorker(data)));
+
+    const merged = results.flat().sort((a, b) => a - b);
+
+    console.log(merged);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await main();
